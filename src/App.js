aec5d67c9f56a7934dbef283272d180cf87d102f
Promtech <uwagboepromise5@gmail.com>
2022-03-13 00:00:00 -0600

    Added Parent.js and it gave me an error, will resolse the error by adding child.js to my components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import BindHandlers from './components/BindHandlers';
+import Parent from './components/Parent';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Greet name = "promise" love = "Coding"/>
       <Welcome name="promise" nickname="Promtech"/>
       <BindHandlers/>
+      <Parent/>
     </div>
   );
 }
---
+++ src/components/Parent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+ class Parent extends Component {
+     constructor(props) {
+       super(props)
+     
+       this.state = {
+          parentName: 'Parent'
+       }
+       this.greetParent = this.greetParent.bind(this)
+     }
+     greetParent(Child){
+         alert(`Hello ${this.state.parentName} from ${Child}`);
+     }
+  render() {
+    return (
+      <div>
+          <Child greetHandler={this.greetParent}/>
+      </div>
+    )
+  }
+}
+
+export default Parent
