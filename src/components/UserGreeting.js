eea267896e6cf5f8e242ef0d1a0e7267c158879e
Promtech <uwagboepromise5@gmail.com>
2022-03-18 00:00:00 -0600

    Added UserGreeting to my components to test for else if rendering

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+    render() {
+        return this.state.isLoggedIn && <div>Welcome Fona</div>
+        // Tenary Operators
+        // return(
+        //     this.state.isLoggedIn?
+        //     <div>Welcome Fona</div>:
+        //     <div>Welcome Guest</div>
+        // )
+        //Element variable 
+        // let message
+        // if(this.state.isLoggedIn){
+        //     message = <div>Welcome Fona</div>
+        // }
+        // else{
+        //     message = <div>Welcome Guest</div>
+        // }
+        // return message
+
+
+        // //IF Else Rendering
+        // if (this.state.isLoggedIn) {
+        //     return (
+        //         <div>Welcome Fona</div>
+        //     )
+        // }
+        // else {
+        //     return (
+        //         <div>Welcome Guest</div>
+        //     )
+        // }
+    }
+}
+
+export default UserGreeting
